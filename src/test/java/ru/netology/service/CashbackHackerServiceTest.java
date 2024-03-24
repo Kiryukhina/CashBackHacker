c33eb603558afb4c7f34588114bf13e4ed216b41
Kiryukhina Ekaterina <kiryuhinakatya@gmail.com>
2024-03-24 19:59:14 +0300

    Add junit4

--- src/test/java/ru/netology/service/CashbackHackerServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackerServiceTest.java
@@ -1,28 +1,25 @@
 package ru.netology.service;
 
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackerServiceTest {
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemain() {
         CashbackHackerService service = new CashbackHackerService();
         int amount = 1000;
         int actual = service.remain(amount);
         int expected = 0;
-        assertEquals(actual, expected);
+        assertEquals(expected , actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemainWithCashback() {
         CashbackHackerService service = new CashbackHackerService();
         int amount = 900;
         int actual = service.remain(amount);
         int expected = 100;
-        assertEquals(actual, expected);
+        assertEquals(expected , actual);
     }
 }
-
-
-
